36498ec1dfdfe1391cee190c7b6bfd2a2dfc84c9
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-08 16:35:26 +0600

    firebase authorization

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import './App.css';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from './firebase.init';
-import { getAuth, signInWithPopup } from 'firebase/auth'
-import { GoogleAuthProvider } from "firebase/auth";
 
-const auth = getAuth(app)
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 function App() {
-  const provider = new GoogleAuthProvider();
-
-  const handleGoogleSignIn = () => {
+  const googleAuthorization = () => {
+    const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
-      .then(
-        (result) => {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          const credential = GoogleAuthProvider.credentialFromResult(result);
-          const token = credential.accessToken;
-          // The signed-in user info.
-          const user = result.user;
-          // ...
-        }).catch((error) => {
-          // Handle Errors here.
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // The email of the user's account used.
-          const email = error.email;
-          // The AuthCredential type that was used.
-          const credential = GoogleAuthProvider.credentialFromError(error);
-          // ...
-        }
-        )
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
   }
   return (
-    <div className="App">
-      <button onClick={handleGoogleSignIn}>Vibe check</button>
+    <div className='App'>
+      <button onClick={googleAuthorization}>Vibe Check</button>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/_steps.js
+++ src/_steps.js
@@ -1,14 +1,14 @@
 /* 
 * Steps to use firebase
 1. Create a project on console.firebase.google.com
-2. Install firebase 
+2. Install firebase with node package manager
 3. Register web app in firebase
-4. Initialize firebase : Copy firebase init with config from firebase project settings in firebase.init.js
+4. Initialize firebase : Copy firebase init with config from firebase project settings in firebase.init.js |Project Settings> SDK setup and configuration|
 5. export default app
 6. import app firebase init.js into app.js
 7. import getAuth from firebase/auth and create auth = getAuth(app)
 8. turn on google authentication (firebase > authentication > enable google sign-in)
 9. create google provider
-10. sign-in with popup
-
+10. useSignInPop-up and pass auth and provider
+11. Handle .then() {if successful} and .catch error {elif error}
 */
