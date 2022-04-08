43f9ceab466b2f08600d5955db5fd9d6c8f121f1
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-08 14:14:05 +0600

    added the steps to connect firebase authentication

---
+++ src/_steps.js
@@ -0,0 +1,11 @@
+/* 
+* Steps to use firebase
+1. Create a project on console.firebase.google.com
+2. Install firebase 
+3. Register web app in firebase
+4. Initialize firebase : Copy firebase init with config from firebase project settings in firebase.init.js
+5. export default app
+6. import app firebase init.js into app.js
+7. import getAuth from firebase/auth and create auth = getAuth(app)
+
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAjR57uol3o7IGPy_mffujonxOufUUE-mg",
+    authDomain: "simple-firebase-authenti-41a94.firebaseapp.com",
+    projectId: "simple-firebase-authenti-41a94",
+    storageBucket: "simple-firebase-authenti-41a94.appspot.com",
+    messagingSenderId: "206295181728",
+    appId: "1:206295181728:web:51c1590f41f5a4cade9cc5",
+    measurementId: "G-8E87KR4HHR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app
